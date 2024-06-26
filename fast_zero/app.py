22f83754995a6a08c13640e1b1d04cc4d3c823c6
SouzaPatrick <souza.felipe.patrick@gmail.com>
2024-06-26 08:02:37 -0300

    Iniciando criacao de usuario

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
+from fast_zero.schemas import Message, UserSchema
+from http import HTTPStatus
 
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/", response_class=Message)
 def read_root():
     return {"message": "Olá Mundo!"}
+
+@app.post("/users", status_code=HTTPStatus.OK, response_class=UserSchema)
+def 
